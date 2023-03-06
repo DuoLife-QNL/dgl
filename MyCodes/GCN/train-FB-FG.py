44906fbeb92fe3721f20677e97c4a5a92c457816
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2023-03-06 14:27:32 +0800

    Test mini-batch DGL-GCN

--- MyCodes/GCN/train-FB-FG.py
+++ MyCodes/GCN/train-FB-FG.py
@@ -10,18 +10,28 @@ import dgl.nn as dglnn
 from dgl import AddSelfLoop
 from dgl.data import CiteseerGraphDataset, CoraGraphDataset, PubmedGraphDataset
 
+PROFILING = False
+DATASET = 'cora'
+SELF_LOOP = True
+NUM_LAYERS = 2
+HIDDEN_CHANNELS = 16
+DROPOUT = 0.5
+BATCH_SIZE = 12
+LR = 0.01
+WEIGHT_DECAY = 0.0
+
 
 class GCN(nn.Module):
     def __init__(self, in_size, hid_size, out_size):
         super().__init__()
         self.layers = nn.ModuleList()
-        # two-layer GCN
-        self.layers.append(
-            dglnn.GraphConv(in_size, hid_size, activation=F.relu)
-        )
+        for _ in range(NUM_LAYERS - 1):
+            self.layers.append(
+                dglnn.GraphConv(in_size, hid_size, activation=F.relu)
+            )
+            in_size = hid_size
         self.layers.append(dglnn.GraphConv(hid_size, out_size))
-        # with torch.profiler.record_function("dropout"):
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(DROPOUT)
 
     def forward(self, g, features):
         h = features
@@ -48,24 +58,29 @@ def train(g, features, labels, masks, model):
     train_mask = masks[0]
     val_mask = masks[1]
     loss_fcn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
 
     # training loop
-    prof = torch.profiler.profile(
-        activities=[
-            torch.profiler.ProfilerActivity.CPU,
-            torch.profiler.ProfilerActivity.CUDA,
-        ],
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=10, repeat=1),
-        # on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/gcn'),
-        record_shapes=True,
-        with_stack=True,
-        # profile_memory=True
-    )
-    prof.start()
+    prof = None
+    if PROFILING:
+        prof = torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.CUDA,
+            ],
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=10, repeat=1),
+            # on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/gcn'),
+            record_shapes=True,
+            with_stack=True,
+            # profile_memory=True
+        )
+        prof.start()
     for epoch in range(30):
         model.train()
-        with torch.profiler.record_function("Forward Computation"):
+        if PROFILING:
+            with torch.profiler.record_function("Forward Computation"):
+                logits = model(g, features)
+        else:
             logits = model(g, features)
         loss = loss_fcn(logits[train_mask], labels[train_mask])
         optimizer.zero_grad()
@@ -77,9 +92,11 @@ def train(g, features, labels, masks, model):
                 epoch, loss.item(), acc
             )
         )
-        prof.step()
-    prof.stop()
-    print(prof.key_averages().table(sort_by="cpu_time_total"))
+        if PROFILING:
+            prof.step()
+    if PROFILING:
+        prof.stop()
+        print(prof.key_averages().table(sort_by="cpu_time_total"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -93,18 +110,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f"Training with DGL built-in GraphConv module.")
 
-    # load and preprocess dataset
-    transform = (
-        AddSelfLoop()
-    )  # by default, it will first remove self-loops to prevent duplication
-    if args.dataset == "cora":
+    transform = None
+    if SELF_LOOP:
+        transform = (
+            AddSelfLoop()
+        )  # by default, it will first remove self-loops to prevent duplication
+    if DATASET == "cora":
         data = CoraGraphDataset(transform=transform)
-    elif args.dataset == "citeseer":
+    elif DATASET == "citeseer":
         data = CiteseerGraphDataset(transform=transform)
-    elif args.dataset == "pubmed":
+    elif DATASET == "pubmed":
         data = PubmedGraphDataset(transform=transform)
-    else:
-        raise ValueError("Unknown dataset: {}".format(args.dataset))
     g = data[0]
     print(type(g))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -123,8 +139,7 @@ if __name__ == "__main__":
     # create GCN model
     in_size = features.shape[1]
     out_size = data.num_classes
-    model = GCN(in_size, 16, out_size).to(device)
-
+    model = GCN(in_size, HIDDEN_CHANNELS, out_size).to(device)
     # model training
     print("Training...")
     train(g, features, labels, masks, model)
@@ -133,3 +148,4 @@ if __name__ == "__main__":
     print("Testing...")
     acc = evaluate(g, features, labels, masks[2], model)
     print("Test accuracy {:.4f}".format(acc))
+    
---
+++ MyCodes/GCN/train-MB-FG.py
@@ -0,0 +1,195 @@
+import argparse
+import profile
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import dgl
+import dgl.nn as dglnn
+from dgl import AddSelfLoop
+from dgl.data import CiteseerGraphDataset, CoraGraphDataset, PubmedGraphDataset, FlickrDataset
+from dgl.dataloading import DataLoader, NeighborSampler, MultiLayerFullNeighborSampler
+
+from torchmetrics.functional.classification import multiclass_f1_score
+
+PROFILING = False
+DATASET = 'flickr'
+SELF_LOOP = True
+NUM_LAYERS = 2
+HIDDEN_CHANNELS = 256
+DROPOUT = 0.3
+BATCH_SIZE = 500
+LR = 0.01
+WEIGHT_DECAY = 0.0
+
+DROP_INPUT = True
+EPOCHS = 400
+
+
+class GCN(nn.Module):
+    def __init__(self, in_size, hid_size, out_size):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for _ in range(NUM_LAYERS - 1):
+            self.layers.append(
+                dglnn.GraphConv(in_size, hid_size, activation=F.relu)
+            )
+            in_size = hid_size
+        self.layers.append(dglnn.GraphConv(hid_size, out_size))
+        self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self, blocks, features):
+        h = features
+        for i, (layer, block) in enumerate(zip(self.layers, blocks)):
+            if i == 0 and DROP_INPUT:
+                h = self.dropout(h)
+            else:
+                h = self.dropout(h)
+            h = layer(block, h)
+            h = F.relu(h)
+        return h
+
+
+def evaluate(model, dataloader):
+    model.eval()
+    ys = []
+    y_hats = []
+    for it, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
+        with torch.no_grad():
+            x = blocks[0].srcdata['feat']
+            ys.append(blocks[-1].dstdata['label'])
+            y_hats.append(model(blocks, x))
+    targets = torch.cat(ys)
+    logits = torch.cat(y_hats)
+    num_classes = logits.shape[1]
+    _, preds = torch.max(logits, dim=1)
+    f1_score = multiclass_f1_score(preds, targets, num_classes=num_classes, average='micro')
+    return f1_score.item()
+
+
+
+def train(g, features, labels, masks, model):
+    # define train/val samples, loss function and optimizer
+    train_mask = masks[0]
+    # get training node indices from train mask
+    train_idx = torch.nonzero(train_mask, as_tuple=False).squeeze()
+    # get validation node indices from val mask
+    val_mask = masks[1]
+    val_idx = torch.nonzero(val_mask, as_tuple=False).squeeze()
+    # get test node indices from test mask
+    test_mask = masks[2]
+    test_idx = torch.nonzero(test_mask, as_tuple=False).squeeze()
+    loss_fcn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+    batch_size = BATCH_SIZE
+    sampler = MultiLayerFullNeighborSampler(NUM_LAYERS, prefetch_node_feats=['feat'], prefetch_labels=['label'])
+    train_dataloader = DataLoader(g, train_idx, sampler, device=device,
+                                  batch_size=batch_size, shuffle=True,
+                                  drop_last=False, num_workers=0)
+    val_dataloader = DataLoader(g, val_idx, sampler, device=device,
+                                batch_size=batch_size, shuffle=True,
+                                drop_last=False, num_workers=0)
+    test_dataloader = DataLoader(g, test_idx, sampler, device=device,
+                                 batch_size=batch_size, shuffle=True,
+                                 drop_last=False, num_workers=0)
+
+    # training loop
+    prof = None
+    if PROFILING:
+        prof = torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.CUDA,
+            ],
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=10, repeat=1),
+            # on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/gcn'),
+            record_shapes=True,
+            with_stack=True,
+            # profile_memory=True
+        )
+        prof.start()
+    best_val_acc = 0
+    cor_test_acc = 0
+    for epoch in range(EPOCHS):
+        
+        model.train()
+        total_loss = 0
+        for it, (input_nodes, output_nodes, blocks) in enumerate(train_dataloader):
+            x = blocks[0].srcdata['feat']
+            y = blocks[-1].dstdata['label']
+            y_hat = model(blocks, x)
+            loss = F.cross_entropy(y_hat, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        val_acc = evaluate(model, val_dataloader)
+        test_acc = evaluate(model, test_dataloader)
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            cor_test_acc = test_acc
+        print("Epoch {:05d} | Loss {:.4f} | Val_Acc {:.4f} | Test_Acc {:.4f}"
+              .format(epoch, total_loss / (it+1), val_acc, test_acc))
+        if PROFILING:
+            prof.step()
+    print("Best Val Acc {:.4f} | Cor Test Acc {:.4f}".format(best_val_acc, cor_test_acc))
+    if PROFILING:
+        prof.stop()
+        print(prof.key_averages().table(sort_by="cpu_time_total"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="citeseer",
+        help="Dataset name ('cora', 'citeseer', 'pubmed').",
+    )
+    parser.add_argument("--gpu", type=int, default=0)
+    args = parser.parse_args()
+    print(f"Training with DGL built-in GraphConv module.")
+
+    transform = None
+    if SELF_LOOP:
+        transform = (
+            AddSelfLoop()
+        )  # by default, it will first remove self-loops to prevent duplication
+    if DATASET == "cora":
+        data = CoraGraphDataset(transform=transform)
+    elif DATASET == "citeseer":
+        data = CiteseerGraphDataset(transform=transform)
+    elif DATASET == "pubmed":
+        data = PubmedGraphDataset(transform=transform)
+    elif DATASET == 'flickr':
+        data = FlickrDataset(transform=transform)
+    g = data[0]
+    print(type(g))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.cuda.set_device(2)
+    g = g.to(device)
+    # g = g.int().to(device)
+    features = g.ndata["feat"]
+    labels = g.ndata["label"]
+    masks = g.ndata["train_mask"], g.ndata["val_mask"], g.ndata["test_mask"]
+
+    # normalization
+    degs = g.in_degrees().float()
+    norm = torch.pow(degs, -0.5).to(device)
+    norm[torch.isinf(norm)] = 0
+    g.ndata["norm"] = norm.unsqueeze(1)
+
+    # create GCN model
+    in_size = features.shape[1]
+    out_size = data.num_classes
+    model = GCN(in_size, HIDDEN_CHANNELS, out_size).to(device)
+    # model training
+    print("Training...")
+    train(g, features, labels, masks, model)
+
+    # test the model
+    # print("Testing...")
+    # acc = evaluate(g, features, labels, masks[2], model)
+    # print("Test accuracy {:.4f}".format(acc))
+    
