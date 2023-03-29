39e90f69a2aad0e136f026d0d318f6266bd99061
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2023-03-29 15:15:12 +0800

    Use original python file Use original python file as import libraries rather than installed one

--- MyCodes/GNNAutoScale/torch_autoscale/__init__.py
+++ MyCodes/GNNAutoScale/torch_autoscale/__init__.py
@@ -5,9 +5,14 @@ import torch
 
 __version__ = '0.0.0'
 
+tmp = '/home/lihz/anaconda3/envs/dgl-src/lib/python3.9/site-packages/torch_autoscale-0.0.0-py3.9-linux-x86_64.egg/torch_autoscale/__init__.py'
+
+# for library in ['_relabel', '_async']:
+#     torch.ops.load_library(importlib.machinery.PathFinder().find_spec(
+#         library, [osp.dirname(__file__)]).origin)
 for library in ['_relabel', '_async']:
     torch.ops.load_library(importlib.machinery.PathFinder().find_spec(
-        library, [osp.dirname(__file__)]).origin)
+        library, [osp.dirname(tmp)]).origin)
 
 # from .data import get_data  # noqa
 from .history import History  # noqa
